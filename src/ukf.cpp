518ccaceb7bc7ac2bb64012b1acf7bef82969d32
Kelly Smith <ksmith433@gatech.edu>
2017-04-10 11:40:32 -0500

    Added test modes for AugmentedSigmaPoints and PredictMeanAndCovariance methods.

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -187,7 +187,36 @@ void UKF::GenerateSigmaPoints(MatrixXd* Xsig_out) {
   *Xsig_out = Xsig;
 }
 
+/**
+  * Generates augmented sigma points.
+  * Unit Test (TestMode=true) indicates it is functioning properly.
+  */
 void UKF::AugmentedSigmaPoints() {
+
+  bool TestMode = false;
+  if (TestMode) {
+    // Override parameters for testing.
+    x_ = VectorXd(n_x_);
+    x_ <<   5.7441,
+         1.3800,
+         2.2049,
+         0.5015,
+         0.3528;
+
+    //create example covariance matrix
+    P_ = MatrixXd(n_x_, n_x_);
+    P_ <<     0.0043,   -0.0013,    0.0030,   -0.0022,   -0.0020,
+            -0.0013,    0.0077,    0.0011,    0.0071,    0.0060,
+             0.0030,    0.0011,    0.0054,    0.0007,    0.0008,
+            -0.0022,    0.0071,    0.0007,    0.0098,    0.0100,
+            -0.0020,    0.0060,    0.0008,    0.0100,    0.0123;
+    //Process noise standard deviation longitudinal acceleration in m/s^2
+    std_a_ = 0.2;
+
+    //Process noise standard deviation yaw acceleration in rad/s^2
+    std_yawdd_ = 0.2;  
+  }
+
   if (DebugMode_) {
     cout << "  a) Generating Augmented Sigma Points" << endl;
   }
@@ -226,11 +255,26 @@ void UKF::AugmentedSigmaPoints() {
 
   //write result
   Xsig_ = Xsig_aug;
+
+  if (TestMode) {
+    cout << "Xsig_aug : " << endl << Xsig_ << endl;
+    /* expected result:
+   Xsig_aug =
+  5.7441  5.85768   5.7441   5.7441   5.7441   5.7441   5.7441   5.7441  5.63052   5.7441   5.7441   5.7441   5.7441   5.7441   5.7441
+    1.38  1.34566  1.52806     1.38     1.38     1.38     1.38     1.38  1.41434  1.23194     1.38     1.38     1.38     1.38     1.38
+  2.2049  2.28414  2.24557  2.29582   2.2049   2.2049   2.2049   2.2049  2.12566  2.16423  2.11398   2.2049   2.2049   2.2049   2.2049
+  0.5015  0.44339 0.631886 0.516923 0.595227   0.5015   0.5015   0.5015  0.55961 0.371114 0.486077 0.407773   0.5015   0.5015   0.5015
+  0.3528 0.299973 0.462123 0.376339  0.48417 0.418721   0.3528   0.3528 0.405627 0.243477 0.329261  0.22143 0.286879   0.3528   0.3528
+       0        0        0        0        0        0  0.34641        0        0        0        0        0        0 -0.34641        0
+       0        0        0        0        0        0        0  0.34641        0        0        0        0        0        0 -0.34641
+*/
+  }
+
 }
 
 /**
   * This predicts the sigma points forward/backward some delta_t. 
-  * This algorithm has a test mode, and testing indicates that it is performing nominally.
+  * Unit Test (TestMode=true) indicates it is functioning properly.
   */
 void UKF::SigmaPointPrediction(double delta_t) {
   // MatrixXd* Xsig_out, 
@@ -315,15 +359,28 @@ void UKF::SigmaPointPrediction(double delta_t) {
 
 /**
   * Computes the mean and covariance of the predicted sigma points.
-  * Probably has a bug in it.
+  * Unit Test (TestMode=true) indicates it is functioning properly.
   */
 void UKF::PredictMeanAndCovariance() {
 
   bool DebugThis = true;
+  bool TestMode = false;
 
   if (DebugMode_) {
     cout << "  c) Predicting mean and covariance" << endl;
   }
+
+  if (TestMode) {
+    // Override variables to perform unit test.
+    Xsig_pred_ <<
+         5.9374,  6.0640,   5.925,  5.9436,  5.9266,  5.9374,  5.9389,  5.9374,  5.8106,  5.9457,  5.9310,  5.9465,  5.9374,  5.9359,  5.93744,
+           1.48,  1.4436,   1.660,  1.4934,  1.5036,    1.48,  1.4868,    1.48,  1.5271,  1.3104,  1.4787,  1.4674,    1.48,  1.4851,    1.486,
+          2.204,  2.2841,  2.2455,  2.2958,   2.204,   2.204,  2.2395,   2.204,  2.1256,  2.1642,  2.1139,   2.204,   2.204,  2.1702,   2.2049,
+         0.5367, 0.47338, 0.67809, 0.55455, 0.64364, 0.54337,  0.5367, 0.53851, 0.60017, 0.39546, 0.51900, 0.42991, 0.530188,  0.5367, 0.535048,
+          0.352, 0.29997, 0.46212, 0.37633,  0.4841, 0.41872,   0.352, 0.38744, 0.40562, 0.24347, 0.32926,  0.2214, 0.28687,   0.352, 0.318159;
+
+  }
+
 
   //create vector for weights
   weights_ = VectorXd(2 * n_aug_ + 1);
@@ -386,6 +443,25 @@ void UKF::PredictMeanAndCovariance() {
   // write result
   x_ = x;
   P_ = P;
+
+  if (TestMode) {
+    cout << "Expected state      = " << x_ << endl;
+    cout << "Expected covariance = " << P_ << endl;
+    /* Expected state     = 5.93637, 1.49035, 2.20528, 0.536853, 0.353577
+       Expected covariance=
+        
+        0.00543425 -0.0024053 0.00341576 -0.00348196 -0.00299378
+        
+        -0.0024053 0.010845 0.0014923 0.00980182 0.00791091
+        
+        0.00341576 0.0014923 0.00580129 0.000778632 0.000792973
+        
+        -0.00348196 0.00980182 0.000778632 0.0119238 0.0112491
+
+        -0.00299378 0.00791091 0.000792973 0.0112491 0.0126972
+    */
+  }
+
 }
 
 /**
